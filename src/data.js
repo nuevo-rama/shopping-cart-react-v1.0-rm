fc29336f59bc00aa21fdba7cbdbbc2af0da78de3
nuevo-rama <you@example.com>
2021-06-18 10:28:29 -0300

    products

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            id: "1",
+            name: "producto 1",
+            price: 2,
+            image: ""
+        },
+        {
+            id: "2",
+            name: "producto 2",
+            price: 4,
+            image: ""
+        },
+        {
+            id: "3",
+            name: "producto 3",
+            price: 6,
+            image: ""
+        }
+    ]
+}
+
+export default data;
